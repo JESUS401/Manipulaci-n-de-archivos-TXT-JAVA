2a431b889f177a5264551e5e306a8ce84c53f9f3
JESUS401 <52077916+JESUS401@users.noreply.github.com>
2021-06-27 00:30:05 -0500

    version_1

---
+++ src/poo3a2/POO3A2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo3a2;
+
+/**
+ *
+ * @author jesus
+ */
+public class POO3A2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ventana1 objeto1 = new ventana1();//Instancio el objeto1 que contiene el método ventana1
+        
+    }
+    
+}
